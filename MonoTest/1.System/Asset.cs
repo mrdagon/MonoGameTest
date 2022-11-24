8e8b18945a96ab0b3adf5d56a6e04e5241bb3603
UEMURA <mr.dagonn@gmail.com>
2022-11-25 08:09:33 +0900

    change

--- MonoTest/1.System/Asset.cs
+++ MonoTest/1.System/Asset.cs
@@ -10,17 +10,32 @@ public class AImage
 {
     public static EnumArray<Frame, int> フレーム = new EnumArray<Frame, int>(9);
 
+    public static Image タイトルロゴ = new Image();
+    public static Image タイトル背景前 = new Image();
+    public static Image タイトル背景後 = new Image();
+
+    public static List<Image> ダンジョン背景 = new List<Image>();
+    public static Image ダンジョン鉱石 = new Image();
+
     public static void Load()
     {
-        フレーム[0].LoadFile("Content/Frame/タブB.png");
-        フレーム[1].LoadFile("Content/Frame/タブBB.png");
-        フレーム[2].LoadFile("Content/Frame/フレームA.png");
-        フレーム[3].LoadFile("Content/Frame/フレームB.png");
-        フレーム[4].LoadFile("Content/Frame/フレームC.png");
-        フレーム[5].LoadFile("Content/Frame/フレームD.png");
-        フレーム[6].LoadFile("Content/Frame/フレームE.png");
-        フレーム[7].LoadFile("Content/Frame/フレームF.png");
-        フレーム[8].LoadFile("Content/Frame/フレームG.png");
+        //タイトル用素材
+        タイトルロゴ.LoadFile("Content/System/title.png");
+        タイトル背景前.LoadFile("Content/Back/middleground.png");
+        タイトル背景後.LoadFile("Content/Back/background.png");
+
+        //ダンジョン用素材
+
+        //フレーム素材
+        フレーム[0].LoadFile("Content/Frame/tab_00.png");
+        フレーム[1].LoadFile("Content/Frame/tab_01.png");
+        フレーム[2].LoadFile("Content/Frame/frame_00.png");
+        フレーム[3].LoadFile("Content/Frame/frame_01.png");
+        フレーム[4].LoadFile("Content/Frame/frame_02.png");
+        フレーム[5].LoadFile("Content/Frame/frame_03.png");
+        フレーム[6].LoadFile("Content/Frame/frame_04.png");
+        フレーム[7].LoadFile("Content/Frame/frame_05.png");
+        フレーム[8].LoadFile("Content/Frame/frame_06.png");
     }
 }
 
@@ -44,10 +59,9 @@ public class ASound
 
     public static void Load()
     {
-        メインBGM.Load("BGM117-110921-yuuyakenagisa");
+        メインBGM.Load("Music/BGM117-110921-yuuyakenagisa");
 
         効果音[SoundType.決定].LoadFile("Content/Sound/loop001.wav");
     }
 
 }
-
